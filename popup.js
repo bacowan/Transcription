9280dfee8ed60359ec0010407c61a8ca43385f3c
bacowan <bacowan@ualberta.ca>
2020-11-21 21:26:32 -0400

    view/hide button

--- popup.js
+++ popup.js
@@ -3,10 +3,15 @@ noSubtitles.style.display = "none";
 
 const viewSubtitlesButton = document.getElementById("view-subtitles-button");
 viewSubtitlesButton.onclick = function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){ 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            {file: "view_subtitles.js"}
+            { file: "view_subtitles.js" },
+            function(ret) { setSubtitlesShowing(ret[0]); }
         );
     });
+}
+
+function setSubtitlesShowing(show) {
+    viewSubtitlesButton.innerText = show ? "Hide" : "View";
 }
--- view_subtitles.js
+++ view_subtitles.js
@@ -1,17 +1,34 @@
-const iframe = document.createElement("iframe");
-const div = document.createElement("div");
-const text = document.createElement("p");
-text.innerText = "HI THERE"
-document.documentElement.appendChild(iframe);
-iframe.contentDocument.body.appendChild(div);
-div.appendChild(text);
-iframe.style.position = "fixed";
-iframe.style.left = 0;
-iframe.style.bottom = 0;
-iframe.style.width = "100%";
-iframe.style.zIndex = "2147483647";
-div.style.background = "black";
-div.style.opacity = "75%";
-div.style.width = "100%";
-div.style.height = "100%";
-text.style.color = "white";
+if (typeof iframe === "undefined") {
+    var iframe;
+}
+
+if (document.documentElement.contains(iframe)) {
+    (function() {
+        if (typeof iframe !== "undefined") {
+            iframe.parentNode.removeChild(iframe);
+        }
+    })();
+    false;
+}
+else {
+    (function() {
+        iframe = document.createElement("iframe");
+        const div = document.createElement("div");
+        const text = document.createElement("p");
+        text.innerText = "HI THERE"
+        document.documentElement.appendChild(iframe);
+        iframe.contentDocument.body.appendChild(div);
+        div.appendChild(text);
+        iframe.style.position = "fixed";
+        iframe.style.left = 0;
+        iframe.style.bottom = 0;
+        iframe.style.width = "100%";
+        iframe.style.zIndex = "2147483647";
+        div.style.background = "black";
+        div.style.opacity = "75%";
+        div.style.width = "100%";
+        div.style.height = "100%";
+        text.style.color = "white";
+    })();
+    true;
+}
